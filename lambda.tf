5ce542367bddf34e363bb154173b756adbda0127
Eichenmann <wollmannmarcel1107@gmail.com>
2023-10-26 11:26:44 +0200

    lambdaUpdate

--- lambda.tf
+++ lambda.tf
@@ -13,3 +13,8 @@ resource "aws_lambda_permission" "sqs_lambda_permission" {
   principal     = "sqs.amazonaws.com"
   source_arn    = aws_sqs_queue.my_sqs_queue.arn
 }
+
+resource "aws_lambda_event_source_mapping" "sqs_lambda_function" {
+  event_source_arn = aws_sqs_queue.my_sqs_queue
+  function_name = aws_lambda_function.sqs_lambda_function
+}
